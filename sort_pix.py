5403c4e00940bedfed729cacdc15f1742ac8ba5e
AtWillB <46000428+AtWillB@users.noreply.github.com>
2021-06-25 13:35:15 -0400

    Update sort_pix.py

--- sort_pix.py
+++ sort_pix.py
@@ -7,11 +7,19 @@
 
 
 #INSTRUCTIONS:
-#1. Place this file in your "rawdata - local" folder. DO NOT PLACE THIS IN THE CLUSTER "rawdata" FOLDER
+#1. Place this file in your "rawdata - local" folder on your desktop. DO NOT PLACE THIS IN THE CLUSTER "rawdata" FOLDER
 #2. edit the file by right-clicking it in File Explorer. 
 #3. Press "edit with IDLE" or "edit with Notepad++"
 #4. Find the Section that says "ONLY EDIT THINGS ..."
-#5. Edit it like we do for the perl script for the x*** folder that you wish to sort
+#5. Edit it like we do for the perl script for  folder that you wish to sort(Ex: x383)
+#6. Go to the search bar in the bottom left and type in "command prompt". Hit enter
+#7. You should get a black terminal window to open. Type the following commands
+#8. "cd Desktop"
+#9. "cd raw" then press tab
+#10. now you are in the proper place to run the script. This window is seperate from the putty window, as that only deals with stuff on the GLAD cluster
+#11. now, type in the name of the script. 
+#12. sort_pix.py and hit enter
+#13. this will run the script. If you want to run it again, just edit the script to the new folder you want to sort and then type in "sort_pix.py" again
 
 import os
 import shutil
